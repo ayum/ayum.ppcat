dbccce9d5c5c95e23336308e3b63fde26baf0069
Aleksey Mikhaylov <almikhayl@gmail.com>
2020-04-03 09:59:42 +0000

    tests: std move for in-place arrays

--- src/ppcat/cli/main.test.cpp
+++ src/ppcat/cli/main.test.cpp
@@ -2,22 +2,19 @@
 
 #include <doctest/doctest.h>
 
+#include <utility>
+
 using namespace ppcat::cli;
 
-template<typename T>
-constexpr inline const T& tmp(const T& t) {
-    return t;
-}
-
 TEST_CASE("testing tests option") {
-    CHECK(main(3, tmp((const char *[]){"ppcat", "tests", "--help", nullptr})) == 0);
+    CHECK(main(3, std::move((const char *[]){"ppcat", "tests", "--help", nullptr})) == 0);
 }
 
 TEST_CASE("testing invalid options") {
-    CHECK(main(2, tmp((const char *[]){"ppcat", "nonexistent-argument", nullptr})) == 109);
-    CHECK(main(3, tmp((const char *[]){"ppcat", "nonexistent-argument", "tests", nullptr})) == 109);
+    CHECK(main(2, std::move((const char *[]){"ppcat", "nonexistent-argument", nullptr})) == 109);
+    CHECK(main(3, std::move((const char *[]){"ppcat", "nonexistent-argument", "tests", nullptr})) == 109);
 }
 
 TEST_CASE("testing valid options") {
-    CHECK(main(2, tmp((const char *[]){"ppcat", "--help", nullptr})) == 0);
+    CHECK(main(2, std::move((const char *[]){"ppcat", "--help", nullptr})) == 0);
 }

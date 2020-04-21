dabdd2063c56c6994e572456b5fdb78e3ce48950
Aleksey Mikhaylov <almikhayl@gmail.com>
2020-04-21 17:13:59 +0000

    napi: first working

--- src/ppcat/napi/main.cc
+++ src/ppcat/napi/main.cc
@@ -1,16 +1,31 @@
-#include <assert.h>
+#include "picker.hpp"
+
 #include <napi.h>
 #include <string>
 #include <string_view>
 
-static Napi::Value ppcat(const Napi::CallbackInfo& info) {
-    Napi::Value ret = info[0];
+using namespace ppcat;
+using namespace nlohmann;
+
+static std::unique_ptr<backend::picker> picker;
+
+static Napi::Value pick(const Napi::CallbackInfo& info) {
+    const Napi::Value &&arg = info[0];
+    if (not arg.IsString()) {
+        throw Napi::Error::New(info.Env(), "First argument of pick method must be string");
+    }
+    std::string input = info[0].As<Napi::String>();
+
+    json data = picker->pick(input);
+
+    Napi::String ret = Napi::String::New(info.Env(), data.dump(2));
     return ret;
 }
 
 static Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  exports["ppcat"] = Napi::Function::New(env, ppcat);
-  return exports;
+    picker.reset();
+    exports["pick"] = Napi::Function::New(env, pick);
+    return exports;
 }
 
 NODE_API_MODULE(ppcat, Init)
